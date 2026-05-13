cd9dab7240bd0612ed4ac19cc740c3827d4313aa
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2026-05-13 12:41:06 +0200

    sc

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ python manage.py collectstatic --noinput
 echo "→ Iniciando Gunicorn..."
 exec gunicorn urlmonitor.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 3 \
+    --workers 1 \
+    --threads 4 \
     --timeout 60 \
     --access-logfile - \
     --error-logfile -
